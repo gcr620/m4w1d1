b48a2756ec93d8889fd6d68d6dc5e14ea4a33e01
gcr620 <148818562+gcr620@users.noreply.github.com>
2024-01-31 23:46:07 +0100

    Add files via upload

---
+++ M4W1D1/script.js
@@ -0,0 +1,71 @@
+// 1
+// function es1() {
+//     let x=3;
+//     let y=2;
+//     if (x===50 || y===50 || x+y===50) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// };
+// console.log(es1());
+
+// 2
+// let string = "Hello";
+// let pos=3;
+// function es2(string, pos) {
+//     for (let index = 0; index <= string.length; index++) {
+//         if (index===pos) {
+//             string.slice(3,3);
+//         } else continue
+//         return string;
+//     }
+// }
+// console.log(es2(string, pos));
+
+// 3
+// function es3() {
+//     let x=40;
+//     let y=70;
+//     if ((x>=40 && x<=60 && y>=40 && y<=60) || (x>=70 && x<=100 && y>=70 && y<=100)) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+// console.log(es3());
+// 4
+// function es4() {
+//     let city="Angeles";
+//     if (city.includes("Los") || city.includes("New")) {
+//         return city;
+//     } else {
+//         return false;
+//     }
+// }
+// console.log(es4());
+// 5
+// function es5() {
+//     let plus = [1,2,3];
+//     let res=0;
+//     for (let index = 0; index < plus.length; index++) {
+//         res += plus[index];
+//     }
+//     return res;
+// }
+// console.log(es5());
+// 6
+// function es6() {
+//     let ar = ["Gab",5,3,"Epicode"];
+//     if (ar.includes(1) || ar.includes(3)) {
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+// console.log(es6());
+// 7
+
+
+
+// 8
